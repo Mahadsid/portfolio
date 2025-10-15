42d917ae59b9b52425727c67aae4ac44123ba657
MSID <muhammadmahad0808@gmail.com>
2025-10-15 22:22:04 +0530

    first commit of portfolio

---
+++ js/projects.js
@@ -0,0 +1,9 @@
+// On page load, smooth scroll to first item
+$(function(){
+    $('html, body').animate({
+        scrollTop: $(".project > .item:nth-child(1)").offset().top - ((window.innerHeight - $(".project > .item:nth-child(1)").height()) / 2)
+    }, 800);
+    return false;
+});
+// set the current year automatically
+document.getElementById("year").textContent = new Date().getFullYear();
